941eb79275ee42ecf5dff701af7944575fdf8778
mahboobgithubac <mahboob.ignou@gmail.com>
2024-03-05 18:31:56 +0530

    first commit

--- composite-key-example/src/main/java/com/example/demo/CompositeKeyExampleApplication.java
+++ composite-key-example/src/main/java/com/example/demo/CompositeKeyExampleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CompositeKeyExampleApplication {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CompositeKeyExampleApplication.class, args);
 	}
